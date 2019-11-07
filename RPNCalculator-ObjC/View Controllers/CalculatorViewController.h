dc38aca74274029882b8f5cf823fbe95a4f3ce86
Dongwoo Pae <dongwoopae@gmail.com>
2019-11-07 17:55:19 +0900

    completed part 1 - Storyboard and organized files

--- RPNCalculator-ObjC/View Controllers/CalculatorViewController.h
+++ RPNCalculator-ObjC/View Controllers/CalculatorViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CalculatorViewController.h
 //  RPNCalculator-ObjC
 //
 //  Created by Dongwoo Pae on 11/7/19.
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
 
+@interface CalculatorViewController : UIViewController
 
 @end
 
